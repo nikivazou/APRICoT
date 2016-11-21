3033d637eeda0c501a4485977bfb80ee88e0907b
Jordan Brown <jmbrown@andrew.cmu.edu>
2016-11-21 13:46:09 -0500

    Fix build error

--- conference-management-system/Foundation.hs
+++ conference-management-system/Foundation.hs
@@ -11,11 +11,15 @@ import Yesod.Auth.Dummy
 import Yesod.Default.Util   (addStaticContentExternal)
 import Yesod.Core.Types     (Logger)
 import Yesod.Auth.OAuth2.Google
-import Yesod.Auth
 import Yesod.Auth.Account
 import qualified Yesod.Core.Unsafe as Unsafe
 import qualified Data.CaseInsensitive as CI
 import qualified Data.Text.Encoding as TE
+import Network.Mail.Mime
+import qualified Data.Text.Lazy.Encoding
+import Text.Shakespeare.Text (stext)
+import Text.Blaze.Renderer.Utf8 (renderHtml)
+import Debug.Trace
 
 -- | The foundation datatype for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
@@ -231,7 +235,7 @@ instance YesodAuth App where
 --       it actually sends an email
 instance AccountSendEmail App
 
-instance YesodAuthAccount (AccountPersistDB MyApp User) App where
+instance YesodAuthAccount (AccountPersistDB App User) App where
     runAccountDB = runAccountPersistDB
 
 -- | Access function to determine if a user is logged in.
