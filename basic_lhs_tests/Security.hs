f679a3e52ed9577a026bde5c3f1efc3b5b4d9849
Alison Kao <ackao@andrew.cmu.edu>
2016-11-28 15:27:55 -0500

    liquidhaaaaskell file started

---
+++ basic_lhs_tests/Security.hs
@@ -0,0 +1,60 @@
+data User = Chair | Alice | Bob | Default
+
+-- | Value tagged with a policy  
+
+{-@ data Tagged a <p :: User->Prop> = Tagged (content :: a) @-}
+data Tagged a = Tagged a
+
+{-@ return :: a -> Tagged a @-}
+return :: a -> Tagged a
+return x = Tagged x 
+
+
+{-@ bind :: forall a b <p :: User -> Prop, f:: a -> b -> Prop>.
+                x: Tagged <p> a
+                -> (u:a -> Tagged <p> (b <f u>))
+                -> Tagged <p> (b<f (content x)>)
+@-}
+bind :: Tagged a -> (a -> Tagged b) -> (Tagged b)
+bind (Tagged x) f = f x
+
+data PaperId
+data World
+
+{- Sensitive data and policies -}
+   
+-- | Current session user
+{-@ measure sessionUser :: World -> User @-}
+sessionUser = Alice
+{-@ assume getSessionUser :: w:World -> {u:User | u == sessionUser w} @-}
+getSessionUser :: World -> User
+getSessionUser w = Alice
+
+-- | PC chair (public)
+{-@ measure chair :: World -> User @-}
+chair = Chair
+{-@ assume getChair :: w:World -> {u:User | chair w == u } @-}
+getChair :: World -> User
+getChair w = Chair
+
+-- | Paper title (public)
+getPaperTitle :: World -> PaperId -> String
+getPaperTitle w pid = "Waow" -- hack for now
+
+-- | Paper authors (visible only to the chair)
+{-@ assume getPaperAuthors :: w: World -> pid : PaperId -> {l: [User] | sessionUser w == chair w} @-}
+getPaperAuthors :: World -> PaperId -> [User]
+getPaperAuthors w pid = [Alice, Bob] -- hack for now
+defaultPaperAuthors = [Default]
+
+{- Client code -}
+{-}  
+-- | Show paper info to session user
+-- | Repair: check that u is chair
+showPaper :: w: World -> pid: PaperId -> World
+showPaper = \w . \pid .
+  let u = getSessionUser w in
+  let title = getPaperTitle w pid in
+  let authors = liftM show (getPaperAuthors w pid) in
+  let out = liftM2 strcat title authors in
+  print w u out -}
